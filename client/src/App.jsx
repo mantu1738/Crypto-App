341004cb56a18ab406f1cdb335dd1b19e7598827
Mantosh Sah <mantoshsah55@gmail.com>
2022-06-07 23:09:16 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { Welcome, Navbar, Services, Transactions, Footer } from "./components";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
